9493753fecb3c506cad695fecedabb57a6ec165d
xerxes <dikihidayat.dh@gmail.com>
2024-12-09 10:02:02 +0700

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,64 @@
+/** @format */
 
-import Header from "@/components/page1/Header"
-import Marque from "@/components/page1/Marquee"
-import PageTwo from "@/components/page2/PageTwo"
-import PageThree from "@/components/page3/PageThree"
-import PageFour from "@/components/page4/PageFour"
-import PageFive from "@/components/page5/PageFive"
-import PageSix from "@/components/page6/PageSix"
-import PageSeven from "@/components/page7/PageSeven"
-import PageEight from "@/components/page8/PageEight"
-import PageNine from "@/components/page9/PageNine"
-import PageTen from "@/components/page10/PageTen"
-import Footer from "@/components/Footer/Footer"
-import Navbar from "@/components/page1/Navbar"
+import Header from "@/components/page1/Header";
+import Marque from "@/components/page1/Marquee";
+import PageTwo from "@/components/page2/PageTwo";
+import PageThree from "@/components/page3/PageThree";
+import PageFour from "@/components/page4/PageFour";
+import PageFive from "@/components/page5/PageFive";
+import PageSix from "@/components/page6/PageSix";
+import PageSeven from "@/components/page7/PageSeven";
+import PageEight from "@/components/page8/PageEight";
+import PageNine from "@/components/page9/PageNine";
+import PageTen from "@/components/page10/PageTen";
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/page1/Navbar";
 
 export default function Page() {
-    return (
-        <>
-            <div className="bg-custom relative min-h-screen">
-                <Navbar />
-                <Header />
-                {/* Marquee dengan posisi absolute di bawah */}
-                <div className="absolute bottom-0 w-full mb-2">
-                    <Marque />
-                </div>
-            </div>
+  return (
+    <>
+      <div className="bg-custom relative min-h-screen">
+        <Navbar />
+        <Header />
+        {/* Marquee dengan posisi absolute di bawah */}
+        <div className="absolute bottom-0 w-full mb-2">
+          <Marque />
+        </div>
+      </div>
 
-            <div className='page-frone'>
-                <PageTwo />
-            </div>
-            <div className='page-two'>
-                <PageThree />
-            </div>
-            <div className='page-two'>
-                <PageFour />
-            </div>
+      <div className="page-frone">
+        <PageTwo />
+      </div>
+      <div className="page-two">
+        <PageThree />
+      </div>
+      <div className="page-two hidden 2xl:block">
+        <PageFour />
+      </div>
 
-            <div className=''>
-                <PageFive />
-            </div>
+      <div className="">
+        <PageFive />
+      </div>
 
-            <div className=''>
-                <PageSix />
-            </div>
+      <div className="">
+        <PageSix />
+      </div>
 
-            <div className=''>
-                <PageSeven />
-            </div>
-            <div className=''>
-                <PageEight />
-            </div>
-            <div className=''>
-                <PageNine />
-            </div>
-            <div className=''>
-                <PageTen />
-            </div>
-            <div className='w-full'>
-                <Footer />
-            </div>
-        </>
-    );
+      <div className="">
+        <PageSeven />
+      </div>
+      <div className="">
+        <PageEight />
+      </div>
+      <div className="hidden">
+        <PageNine />
+      </div>
+      <div className="">
+        <PageTen />
+      </div>
+      <div className="w-full">
+        <Footer />
+      </div>
+    </>
+  );
 }
--- src/components/page4/PageFour.tsx
+++ src/components/page4/PageFour.tsx
@@ -1,185 +1,189 @@
-'use client';
+/** @format */
+
+"use client";
 
 import React from "react";
-import {
-    motion
-
-} from "framer-motion"
-
+import { motion } from "framer-motion";
 
 export default function PageFour() {
-    const images = [
-        '/pagefourasset/coid.png',
-        '/pagefourasset/com.png',
-        '/pagefourasset/id.png',
-        '/pagefourasset/info.png',
-        '/pagefourasset/net.png',
-        '/pagefourasset/org.png',
-        '/pagefourasset/org.png',
-        '/pagefourasset/site.png',
-        '/pagefourasset/web.png',
-        '/pagefourasset/info.png',
-        '/pagefourasset/coid.png',
-        '/pagefourasset/com.png',
-    ];
+  const images = [
+    "/pagefourasset/coid.png",
+    "/pagefourasset/com.png",
+    "/pagefourasset/id.png",
+    "/pagefourasset/info.png",
+    "/pagefourasset/net.png",
+    "/pagefourasset/org.png",
+    "/pagefourasset/org.png",
+    "/pagefourasset/site.png",
+    "/pagefourasset/web.png",
+    "/pagefourasset/info.png",
+    "/pagefourasset/coid.png",
+    "/pagefourasset/com.png",
+  ];
 
-    return (
-        <div className="relative flex justify-center items-center h-screen overflow-hidden 2xl:mt-12 xl:mt-12 lg:mt-12 md:mt-12 sm:mt-2 xs:mt-12">
-            {/* Concentric Circles */}
-            {/* Concentric Circles */}
-            {[...Array(12)].map((_, i) => {
-                // Define specific angles for each image position
-                const angles = [null, null, 320, 190, 40, 320, 150, 200, 20, 170, 190];
-                const angle = angles[i] || 0; // Default to 0 if there aren't enough angles
-                const imageSrc = images[i % images.length]; // Cycle through images
-                if (angles[i] === null) {
-                    return (
-                        <div
-                            key={i}
-                            className="absolute rounded-full border border-gray-800"
-                            style={{
-                                width: `${150 + i * 150}px`, // Adjust for spacing between circles
-                                height: `${150 + i * 150}px`,
-                                zIndex: 1,
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                            }}
-                        >
-                        </div>
-                    )
-                }
+  return (
+    <div className="relative flex justify-center items-center h-screen overflow-hidden 2xl:mt-12 xl:mt-12 lg:mt-12 md:mt-12 sm:mt-2 xs:mt-12">
+      {/* Concentric Circles */}
+      {/* Concentric Circles */}
+      {[...Array(12)].map((_, i) => {
+        // Define specific angles for each image position
+        const angles = [null, null, 320, 190, 40, 320, 150, 200, 20, 170, 190];
+        const angle = angles[i] || 0; // Default to 0 if there aren't enough angles
+        const imageSrc = images[i % images.length]; // Cycle through images
+        if (angles[i] === null) {
+          return (
+            <div
+              key={i}
+              className="absolute rounded-full border border-gray-800"
+              style={{
+                width: `${150 + i * 150}px`, // Adjust for spacing between circles
+                height: `${150 + i * 150}px`,
+                zIndex: 1,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            ></div>
+          );
+        }
 
-                if (angles[i] === 170) {
-                    return (
-                        <motion.div
-                            initial={{
-                                scale: 0, // Memulai sedikit lebih besar
-                                opacity: 0.5, // Awalnya transparan
-                            }}
-                            whileInView={{
-                                scale: 1, // Kembali ke ukuran normal
-                                opacity: 1, // Menjadi tidak transparan
-                            }}
-                            transition={{
-                                scale: {
-                                    type: "spring", // Efek pegas untuk transisi halus
-                                    stiffness: 100, // Stiffness untuk kontrol efek pegas
-                                    damping: 20, // Pengurangan kecepatan pegas
-                                },
-                                opacity: {
-                                    duration: 0.3, // Durasi transisi opacity
-                                }
-                            }}
-                            viewport={{
-                                margin: "-20%", // Menyesuaikan margin viewport
-                            }}
-                            key={i}
-                            className="absolute rounded-full border border-gray-800"
-                            style={{
-                                width: `${150 + i * 150}px`, // Adjust for spacing between circles
-                                height: `${150 + i * 150}px`,
-                                zIndex: 1,
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                            }}
-                        >
-                            {/* Position Image at Specific Angle without rotating it */}
-                            <img
+        if (angles[i] === 170) {
+          return (
+            <motion.div
+              initial={{
+                scale: 0, // Memulai sedikit lebih besar
+                opacity: 0.5, // Awalnya transparan
+              }}
+              whileInView={{
+                scale: 1, // Kembali ke ukuran normal
+                opacity: 1, // Menjadi tidak transparan
+              }}
+              transition={{
+                scale: {
+                  type: "spring", // Efek pegas untuk transisi halus
+                  stiffness: 100, // Stiffness untuk kontrol efek pegas
+                  damping: 20, // Pengurangan kecepatan pegas
+                },
+                opacity: {
+                  duration: 0.3, // Durasi transisi opacity
+                },
+              }}
+              viewport={{
+                margin: "-20%", // Menyesuaikan margin viewport
+              }}
+              key={i}
+              className="absolute rounded-full border border-gray-800"
+              style={{
+                width: `${150 + i * 150}px`, // Adjust for spacing between circles
+                height: `${150 + i * 150}px`,
+                zIndex: 1,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {/* Position Image at Specific Angle without rotating it */}
+              <img
+                src={imageSrc}
+                className="w-12 h-12 rounded-full absolute"
+                style={{
+                  transform: `rotate(${angle}deg) translate(${
+                    (150 + i * 150) / 2
+                  }px) rotate(${-angle}deg)`, // Cancels the rotation effect
+                }}
+              />
 
-                                src={imageSrc}
-                                className="w-12 h-12 rounded-full absolute"
-                                style={{
-                                    transform: `rotate(${angle}deg) translate(${(150 + i * 150) / 2}px) rotate(${-angle}deg)`, // Cancels the rotation effect
-                                }}
-                            />
+              <img
+                src={imageSrc}
+                className="w-12 h-12 rounded-full absolute"
+                style={{
+                  transform: `rotate(${angle + 180}deg) translate(${
+                    (150 + i * 150) / 2
+                  }px) rotate(${-angle + 180}deg)`, // Cancels the rotation effect
+                }}
+              />
+            </motion.div>
+          );
+        }
+        return (
+          <motion.div
+            initial={{
+              scale: 0, // Memulai sedikit lebih besar
+              opacity: 0.5, // Awalnya transparan
+            }}
+            whileInView={{
+              scale: 1, // Kembali ke ukuran normal
+              opacity: 1, // Menjadi tidak transparan
+            }}
+            transition={{
+              scale: {
+                type: "spring", // Efek pegas untuk transisi halus
+                stiffness: 100, // Stiffness untuk kontrol efek pegas
+                damping: 20, // Pengurangan kecepatan pegas
+              },
+              opacity: {
+                duration: 0.3, // Durasi transisi opacity
+              },
+            }}
+            viewport={{
+              margin: "-20%", // Menyesuaikan margin viewport
+            }}
+            key={i}
+            className="absolute rounded-full border border-gray-800"
+            style={{
+              width: `${150 + i * 150}px`, // Adjust for spacing between circles
+              height: `${150 + i * 150}px`,
+              zIndex: 1,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {/* Position Image at Specific Angle without rotating it */}
+            <img
+              src={imageSrc}
+              className="2xl:w-12 xl:w-12 lg:w-12 md:w-12 sm:w-12 xs:w-12 2xl:h-12 xl:h-12 lg:h-12 md:h-12 sm:h-12 xs:h-12 rounded-full absolute"
+              style={{
+                transform: `rotate(${angle}deg) translate(${
+                  (150 + i * 150) / 2
+                }px) rotate(${-angle}deg)`, // Cancels the rotation effect
+              }}
+            />
+          </motion.div>
+        );
+      })}
 
-                            <img
-                                src={imageSrc}
-                                className="w-12 h-12 rounded-full absolute"
-                                style={{
-                                    transform: `rotate(${angle + 180}deg) translate(${(150 + i * 150) / 2}px) rotate(${-angle + 180}deg)`, // Cancels the rotation effect
-                                }}
-                            />
-                        </motion.div>
-                    )
-                }
-                return (
-                    <motion.div
-                        initial={{
-                            scale: 0, // Memulai sedikit lebih besar
-                            opacity: 0.5, // Awalnya transparan
-                        }}
-                        whileInView={{
-                            scale: 1, // Kembali ke ukuran normal
-                            opacity: 1, // Menjadi tidak transparan
-                        }}
-                        transition={{
-                            scale: {
-                                type: "spring", // Efek pegas untuk transisi halus
-                                stiffness: 100, // Stiffness untuk kontrol efek pegas
-                                damping: 20, // Pengurangan kecepatan pegas
-                            },
-                            opacity: {
-                                duration: 0.3, // Durasi transisi opacity
-                            }
-                        }}
-                        viewport={{
-                            margin: "-20%", // Menyesuaikan margin viewport
-                        }}
-                        key={i}
-                        className="absolute rounded-full border border-gray-800"
-                        style={{
-                            width: `${150 + i * 150}px`, // Adjust for spacing between circles
-                            height: `${150 + i * 150}px`,
-                            zIndex: 1,
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        {/* Position Image at Specific Angle without rotating it */}
-                        <img
-                            src={imageSrc}
-                            className="2xl:w-12 xl:w-12 lg:w-12 md:w-12 sm:w-12 xs:w-12 2xl:h-12 xl:h-12 lg:h-12 md:h-12 sm:h-12 xs:h-12 rounded-full absolute"
-                            style={{
-                                transform: `rotate(${angle}deg) translate(${(150 + i * 150) / 2}px) rotate(${-angle}deg)`, // Cancels the rotation effect
-                            }}
-                        />
-                    </motion.div>
-                );
-            })}
+      {/* Center Content */}
+      <div className="relative z-10 flex flex-col items-center text-center p-6 bg-transparent rounded-lg shadow-lg max-w-lg">
+        <img
+          src={"/pagefourasset/ligal.png"}
+          alt="Centered Image"
+          className="w-20 h-20 rounded-full mb-4" // Add spacing below image if needed
+        />
 
+        <p className="2xl:text-3xl xl:text-2xl lg:text-2xl md:text-2xl sm:text-2xl font-semibold text-slate-200 xs:text-2xl">
+          Mengapa Bisnis Anda Butuh
+        </p>
+        <p className="text-3xl font-semibold inline-block bg-gradient-to-r from-white to-sky-500 bg-clip-text text-transparent whitespace-nowrap mb-2">
+          Sentuhan Digital
+        </p>
 
+        <p className="text-slate-200 mb-1 2xl:text-sm xl:text-sm lg:text-sm md:text-xs xs:text-xs sm:text-sm">
+          Di era digital saat ini, konsumen lebih sering mencari produk dan jasa
+          melalui internet. Bisnis yang memiliki website akan lebih mudah
+          ditemukan dan dipercaya oleh pelanggan, karena website berfungsi
+          seperti toko atau kantor online yang selalu siap melayani selama 24
+          jam tanpa henti. Dengan website, bisnis Anda selalu terbuka dan siap
+          menyambut peluang kapan saja
+        </p>
 
-
-            {/* Center Content */}
-            <div className="relative z-10 flex flex-col items-center text-center p-6 bg-transparent rounded-lg shadow-lg max-w-lg">
-                <img
-                    src={'/pagefourasset/ligal.png'}
-                    alt="Centered Image"
-                    className="w-20 h-20 rounded-full mb-4" // Add spacing below image if needed
-                />
-
-                <p className="2xl:text-3xl xl:text-2xl lg:text-2xl md:text-2xl sm:text-2xl font-semibold text-slate-200 xs:text-2xl">Mengapa Bisnis Anda Butuh</p>
-                <p className="text-3xl font-semibold inline-block bg-gradient-to-r from-white to-sky-500 bg-clip-text text-transparent whitespace-nowrap mb-2">
-                    Sentuhan Digital
-                </p>
-
-                <p className="text-slate-200 mb-1 2xl:text-sm xl:text-sm lg:text-sm md:text-xs xs:text-xs sm:text-sm">
-                    Di era digital saat ini, konsumen lebih sering mencari produk dan jasa melalui internet. Bisnis
-                    yang memiliki website akan lebih mudah ditemukan dan dipercaya oleh pelanggan, karena website
-                    berfungsi seperti toko atau kantor online yang selalu siap melayani selama 24 jam tanpa henti.
-                    Dengan website, bisnis Anda selalu terbuka dan siap menyambut peluang kapan saja
-                </p>
-
-                <p className="text-slate-400 mb-4">Temukan solusi digital untuk bisnis Anda</p>
-                <button className="bg-transparent text-white px-8 py-2 rounded-md border border-sky-500 shadow-md transition duration-300 ease-in-out transform hover:scale-105">
-                    Explore
-                </button>
-            </div>
-
-        </div>
-    );
+        <p className="text-slate-400 mb-4">
+          Temukan solusi digital untuk bisnis Anda
+        </p>
+        <button className="bg-transparent text-white px-8 py-2 rounded-md border border-sky-500 shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+          Explore
+        </button>
+      </div>
+    </div>
+  );
 }
