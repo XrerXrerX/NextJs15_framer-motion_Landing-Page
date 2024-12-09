e7b5187be0bb4596bece8e9d0a4d0c4b82aaabad
xerxes <dikihidayat.dh@gmail.com>
2024-12-09 10:31:13 +0700

    push

--- src/components/page1/Marquee.tsx
+++ src/components/page1/Marquee.tsx
@@ -1,41 +1,51 @@
+/** @format */
+
 import { div } from "framer-motion/client";
 import Marquee from "react-fast-marquee";
 
 export default function Marque() {
-    const logos = [
-        '/react.png',
-        '/add.png',
-        '/edd.png',
-        '/udd.png',
-        '/figma.png',
-        '/css.png',
-        '/html5.png',
-        '/vector.png',
-        '/tailwind.png',
-    ];
+  const logos = [
+    "/react.png",
+    "/add.png",
+    "/edd.png",
+    "/udd.png",
+    "/figma.png",
+    "/css.png",
+    "/html5.png",
+    "/Vector.png",
+    "/tailwind.png",
+  ];
 
-    return (
-        <div className="flex flex-col ">
-            {/* Konten lainnya di atas */}
-            <div className="flex-grow">
-                {/* Elemen lain di atas */}
-                <p className="text-white text-center mb-4">
-                    Teknologi Yang Kami Gunakan
-                </p>
-                <div className="w-full bg-dark dark:bg-[#1a1a1a] mt-6">
-                    <div className="relative overflow-hidden w-full mx-auto flex flex-col justify-center items-start h-full">
-                        <Marquee speed={30} gradient={true} gradientColor="rgba(40, 173, 250, 1)" autoFill={true}>
-                            <div className="flex justify-between">
-                                {logos.map((url, index) => (
-                                    <div className="flex flex-col items-center justify-center h-full max-w-[150px] mr-28" key={index}>
-                                        <img src={url} alt={`Skill ${index + 1}`} />
-                                    </div>
-                                ))}
-                            </div>
-                        </Marquee>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col ">
+      {/* Konten lainnya di atas */}
+      <div className="flex-grow">
+        {/* Elemen lain di atas */}
+        <p className="text-white text-center mb-4">
+          Teknologi Yang Kami Gunakan
+        </p>
+        <div className="w-full bg-dark  mt-6">
+          <div className="relative overflow-hidden w-full mx-auto flex flex-col justify-center items-start h-full">
+            <Marquee
+              speed={30}
+              gradient={true}
+              gradientColor="rgba(40, 173, 250, 1)"
+              autoFill={true}
+            >
+              <div className="flex justify-between">
+                {logos.map((url, index) => (
+                  <div
+                    className="flex flex-col items-center justify-center h-auto max-w-[150px] mr-28"
+                    key={index}
+                  >
+                    <img src={url} alt={`Skill ${index + 1}`} />
+                  </div>
+                ))}
+              </div>
+            </Marquee>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
